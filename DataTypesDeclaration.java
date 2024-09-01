03ac5fdd2cad97cff62cdae1543247306211a243
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-09-01 17:06:21 -0600

    Create DataTypesDeclaration.java
    Java Data Type declaration

---
+++ DataTypesDeclaration.java
@@ -0,0 +1,17 @@
+class a2DataTypes {
+    public static void main(String[] args) {
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l;
+       
+        float  f = 5.8f;
+        double d = 5.9;
+
+
+        char c = 'K';
+
+
+        boolean b = true;
+    }
+}
