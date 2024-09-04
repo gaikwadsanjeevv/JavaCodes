dc9a5bf4a2d0a9f2ff30e2b8dd536af74075849d
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-09-04 15:55:45 -0600

    Create WhileLoop.java
    While Loop

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int count = 1; // Initialize counter
+
+        // While loop to print numbers from 1 to 5
+        while (count <= 5) {
+            System.out.println("Number: " + count);
+            count++; // Increment the counter
+        }
+    }
+}
