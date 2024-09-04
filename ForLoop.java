e65ac552f0b4cf40065cd6459874fc09ab08d064
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-09-04 15:59:50 -0600

    Create ForLoop.java
    for loop

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // For loop to print numbers from 1 to 5
+        for (int count = 1; count <= 5; count++) {
+            System.out.println("Number: " + count);
+        }
+    }
+}
