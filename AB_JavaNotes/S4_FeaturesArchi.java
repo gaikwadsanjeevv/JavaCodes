f50a3a0a1d207687f4151e655d7e1a43687b01bb
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2025-02-22 18:58:03 +0000

    feature

---
+++ AB_JavaNotes/S4_FeaturesArchi.java
@@ -0,0 +1,16 @@
+// Interpreter              and          compiler
+//Needs every time goes line by line  | need one time as it compiles the whole programme at ones.
+//Will not generate a sperate file    | Compiler will generate a seperate file.
+//JavaScript(hybrid com as well Inter)| c, c++, Java
+//When code is interpreded each line is read and converted to machine
+// code and excecuted at same time | It will compile whole file if no error it will generate its byte code.
+//Transalation and execution at same time line by line | Traslation is done once when there are no error.
+//Easy to learn but slow | Fast
+//-----------------------------------
+
+//First.java  --> First.class(compiled into byte code by javac) --> JVM(Interpreter translates into machine code-slower) --> So java is hybrid language compiled as well intertreted while compiler generated the new file and interpretor wont.
+
+//The bytecode can run on any platform that has a compatible Java Virtual Machine (JVM), enhancing Java's "write once, run anywhere" philosophy.
+
+//The fact that it starts with a compilation step into bytecode is significant. Therefore, it’s common to call Java a compiled language, even though its execution mechanism has hybrid traits.
+
